2d06c7ce322f284317b8d02951f96e20b9853508
akashgarg-13 <gargakash.1398@gmail.com>
2022-03-19 17:11:16 +0530

    Rust

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+// fn main() {
+//     println!("Hello, world!");
+//     another_function();
+//     second_function(9,8)
+// }
+// fn another_function(){
+//     println!("Its another function");
+// }
+// fn second_function(x:i32,y:i32){
+//     println!("The value of x & y is {} , {} respectively .",x,y);
+// }
+fn main() {
+    let mut s1 = String::from("hello");
+    s1.push_str("world");
+    println!("{}", s1);
+
+    let s2 = s1.clone();
+    println!("string 2 is {}", s2);
+
+    let s3 = s1.len();
+    println!("{}", s3);
+    // let s = String::from("HI I am Akash");
+
+    // let len = calculate_length(&s);
+    // println!("{} {}", s, len);
+
+
+
+        let my_name = "AkashGarg";
+        let my_country = "India";
+        let my_home = "Saharanpur";
+    
+        let together = format!(
+            "I am {} and I come from {} but I live in {}.",
+            my_name, my_country, my_home
+        );
+    
+        println!("{}",together);
+    
+
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+
+    let person1 = Person {
+        name : String::from("Akash"),
+        age : 23,
+        height : 167.0,
+    };
+
+    println!("Person1 details {:?} {:?} {:?}", person1.age , person1.height ,person1.name);
+
+    let rect1 = Rectangle {
+        length:30,
+        width:50
+    };
+
+    println!("Area of rectangle is {:?}",rect1.area());
+// }
+}
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u16,
+    height: f32,
+}
+
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+}
+
+
+
+/*Topics covered ---------------
+
+1. String
+2.struct
+3. Function 
+
+*/
